30618dc59bfebaf0ded9628ed264a6e3bb1b55af
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2022-01-18 21:00:54 -0500

    create initial print statement

---
+++ main.py
@@ -0,0 +1,9 @@
+""" Prints info about the users """
+
+users = [
+  { "first_name": "Rebekah", "last_name": "CK", "user_id": 1 },
+  { "first_name": "Galata", "last_name": "Tona", "user_id": 2 },  
+]
+
+# Print first user's full name
+print(f"{users[1]["first_name"]} {users[1]["last_name"]}")
