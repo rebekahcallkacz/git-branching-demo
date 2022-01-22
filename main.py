985114f1bf74d580245f457002911b34e481c6b9
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2022-01-22 13:11:22 -0500

    test code

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 """ Prints info about the users """
 
 users = [
-  { "first_name": "Rebekah", "last_name": "CK", "user_id": 1 },
-  { "first_name": "Galata", "last_name": "Tona", "user_id": 2 },  
+  { 'first_name': 'Rebekah', 'last_name': 'CK', 'user_id': 1 },
+  { 'first_name': 'Galata', 'last_name': 'Tona', 'user_id': 2 },  
 ]
 
 # Print first user's full name
-print(f"{users[1]["first_name"]} {users[1]["last_name"]}")
+first_user = users[0]
+print(f"{first_user['first_name']} {first_user['last_name']}")
+
+# Print last user's full name
